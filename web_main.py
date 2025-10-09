86439d0a7fc7841382edf56a40d02b09fd7b1239
poly-new <wuxiancai1978@gmail.com>
2025-10-09 08:48:22 +0800

    Update web_main.py

--- web_main.py
+++ web_main.py
@@ -304,7 +304,7 @@ def create_app(engine: TradingEngine, port: int, tz_offset: int, events_q: queue
                 <p>
                   交易类型: <code>${t.test_mode?'模拟':'真实'}</code> · 保证金余额:<code>${t.initial_balance}</code> · 开仓比例:<code>${(Number(t.percent)*100).toFixed(0)}%</code> · 杠杆:<code>${t.leverage}x</code> · 手续费率:<code>${(Number(t.fee_rate)*100).toFixed(3)}%</code> · 交易币对:<code>${t.symbol}</code> · ｜ K线周期:<code>${t.interval}</code>
                   指标: EMA<code>${i.ema_period}</code> · MA<code>${i.ma_period}</code> · K线收盘后交易:<code>${fmtBool(i.use_closed_only)}</code> · EMA/MA斜率约束:<code>${fmtBool(i.use_slope)}</code> · 价格轮询:<code>${fmtBool(w.enable_price_poller)}</code>
-                  当前显示时区:UTC +<code>${w.timezone_offset_hours||0}H</code>
+                  当前显示时区:UTC +<code>${w.timezone_offset_hours||0}</code>
                 </p>
               `;
             }
